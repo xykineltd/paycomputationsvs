38175850131daa532d863a2822c27b7fb91b625b
oyewolemoruff <morufoye@gmail.com>
2024-02-20 20:57:18 +0000

    continued payroll calculation implementation

--- src/main/java/com/xykine/computation/request/PaymentComputeRequest.java
+++ src/main/java/com/xykine/computation/request/PaymentComputeRequest.java
@@ -3,11 +3,13 @@ package com.xykine.computation.request;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class PaymentComputeRequest {
     private String companyId;
     private String departmentId;
+    private List<String> employeeIds;
     private Date start;
     private Date end;
 }
