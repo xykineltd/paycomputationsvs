7459bc8897a3aeee7450eddf21feb5e6dfd8fb0b
oyewolemoruff <morufoye@gmail.com>
2024-03-23 21:43:52 +0000

    further performance enhancement

--- src/main/java/com/xykine/computation/service/ComputeService.java
+++ src/main/java/com/xykine/computation/service/ComputeService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 import java.util.stream.Collectors;
@@ -62,24 +63,42 @@ public class ComputeService {
     }
 
     public List<PaymentInfo> generateReport(List<PaymentInfo> rawInfo){
+        int size = rawInfo.size();
+        List<PaymentInfo> job1 = new ArrayList<>();
+        List<PaymentInfo> job2 = new ArrayList<>();
+          job1.addAll(rawInfo.subList(0, size/2));
+        job2.addAll(rawInfo.subList(size/2, size));
+
         Executor executor = Executors.newFixedThreadPool(10);
-        CompletableFuture<List<PaymentInfo>> processReportFuture = CompletableFuture.supplyAsync(() -> {
-          return  rawInfo.stream()
-                    .filter(x -> x.getEmployee() != null)
-                    .map(x -> paymentCalculator.computeGrossPay(x))
-                    .map(x -> paymentCalculator.computeNonTaxableIncomeExempt(x))
-                    .map(x -> paymentCalculator.prorateEarnings(x))
-                    .map(x -> paymentCalculator.computePayeeTax(x))
-                    .map(x -> paymentCalculator.computeTotalDeduction(x))
-                    .map(x -> paymentCalculator.computeNetPay(x))
-                    .collect(Collectors.toList());
-        }, executor).thenApply(result -> {
-            return result;
+        CompletableFuture<List<PaymentInfo>> job1Future = CompletableFuture.supplyAsync(() -> {
+            return  processReport(job1);
+        }, executor);
+
+        Executor executor2 = Executors.newFixedThreadPool(10);
+        CompletableFuture<List<PaymentInfo>> job2Future = CompletableFuture.supplyAsync(() -> {
+            return  processReport(job2);
+        }, executor2);
+
+        CompletableFuture<List<PaymentInfo>> processReportFuture =job1Future.thenCombine(job2Future, (x, y) -> {
+            x.addAll(y);
+            return x;
         });
         try {
             return processReportFuture.get();
         } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private List<PaymentInfo> processReport(List<PaymentInfo> job){
+        return  job.stream()
+                .filter(x -> x.getEmployee() != null)
+                .map(x -> paymentCalculator.computeGrossPay(x))
+                .map(x -> paymentCalculator.computeNonTaxableIncomeExempt(x))
+                .map(x -> paymentCalculator.prorateEarnings(x))
+                .map(x -> paymentCalculator.computePayeeTax(x))
+                .map(x -> paymentCalculator.computeTotalDeduction(x))
+                .map(x -> paymentCalculator.computeNetPay(x))
+                .collect(Collectors.toList());
     }
 }
