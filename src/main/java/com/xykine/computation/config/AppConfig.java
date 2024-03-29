477eac712ab816a004cc01c4fdf38f7de21123f2
oyewolemoruff <morufoye@gmail.com>
2024-03-29 12:14:29 +0000

    fixed back pressure issue

--- src/main/java/com/xykine/computation/config/AppConfig.java
+++ src/main/java/com/xykine/computation/config/AppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -13,11 +14,20 @@ public class AppConfig {
     @Value("${admin.service.url}")
     private String adminServiceUrl;
 
+    @Value("${admin.service.maxBufferSize}")
+    private Integer maxBufferSize;
+
     @Bean
     WebClient webClient(WebClient.Builder webClientBuilder) {
         var mediaType = MediaType.APPLICATION_JSON_VALUE;
         return webClientBuilder
                 .baseUrl(adminServiceUrl)
+                .exchangeStrategies(ExchangeStrategies
+                        .builder()
+                        .codecs(codecs -> codecs
+                                .defaultCodecs()
+                                .maxInMemorySize(maxBufferSize * 1024))
+                        .build())
                 .defaultHeader("Accept", mediaType)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
