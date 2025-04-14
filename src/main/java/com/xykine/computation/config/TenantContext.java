d1b83e07789b7d56badc0c1fd5bbaf19d9fe698e
Babatunde Alaraje <babatunde.alaraje@karsun-llc.com>
2025-04-14 17:37:03 -0500

    update

--- src/main/java/com/xykine/computation/config/TenantContext.java
+++ src/main/java/com/xykine/computation/config/TenantContext.java
@@ -4,11 +4,11 @@ public class TenantContext {
     private static final ThreadLocal<String> tenantId = new ThreadLocal<>();
 
     public static void setTenantId(String id) {
-        tenantId.set(id != null ? id : "xykine");
+        tenantId.set(id != null ? id : "aced");
     }
 
     public static String getTenantId() {
-        return tenantId.get() != null ? tenantId.get() : "xykine";
+        return tenantId.get() != null ? tenantId.get() : "aced";
     }
 
     public static void clear() {
