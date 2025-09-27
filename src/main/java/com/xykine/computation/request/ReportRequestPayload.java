1937e160669afe7f1a2c69cd0a20819501e503a0
oyewolemoruff <morufoye@gmail.com>
2025-09-27 17:19:16 +0100

    returned report excel for download

--- src/main/java/com/xykine/computation/request/ReportRequestPayload.java
+++ src/main/java/com/xykine/computation/request/ReportRequestPayload.java
@@ -6,10 +6,35 @@ import java.util.List;
 
 @Data
 public class ReportRequestPayload {
-    private boolean isAllEmployee;
-    private List<String> employeeIds;
-    private List<String> selectedHeader;
-
-    private String companyId;
-    private String reportId;
+    private boolean isAll;
+    private List<String> ids;
+    private List<String> headers;
+    private String companyID;
+    private String entityType;
+    private DateRange dateRange;
+    private String docType;
 }
+
+/*
+{
+  "isAll": true,
+  "ids": [],
+  "companyID": "68d6276a019e0574895914dd",
+  "headers": ["financeCode", "glAccountNumber", "glAccountDescription", "statementType", "amount"],
+  "entity": "Finance",
+  "entityType": "Regular",
+  "dateRange": { "fromDate": "2025-09-01", "endDate": "2025-09-30" },
+  "docType": "xcell"
+}
+
+
+#  {
+# isAll: Boolean
+# Ids: string[],
+# Headers: string[]
+# Entity: Payroll / Employee/ Finance / FinanceDetail / PayrollDetail,
+# EntityType: Regular/ Offcycle / Payslip
+# DateRange: { fromDate, endDate }
+# docType: pdf /  xcell
+# }
+* */
