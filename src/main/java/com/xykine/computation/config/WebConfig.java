874396209e8508c63464247a78fe61c8397fbe2f
Xykine Limited <admin@xykine.com>
2025-09-29 19:21:54 -0500

    update

--- src/main/java/com/xykine/computation/config/WebConfig.java
+++ src/main/java/com/xykine/computation/config/WebConfig.java
@@ -15,9 +15,14 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        System.out.println("uri-->" + uri);
         registry.addMapping("/**") // Apply CORS to all endpoints
-                .allowedOrigins(uri) // Allow requests from this origin
+                .allowedOrigins(uri)
+
+//                .allowedOriginPatterns("http://localhost:3000") // Allow requests from this origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
-                .allowedHeaders("*"); // Allowed headers
+                .allowedHeaders("*") // Allowed headers
+                .allowCredentials(true);
+
     }
 }
